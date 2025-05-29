f6b4c3f3c554ea6feffc0029f79bb34b807609a0
Fábio Yoshida <90954129+FabioYoshi@users.noreply.github.com>
2025-05-29 10:34:55 -0300

    Segundo Commit: Armazenamento em Listas
    Código completo, com a parte de armazenamento em lista e analise

--- 25.1.GS.Fabio.Felipe.Tiago.py
+++ 25.1.GS.Fabio.Felipe.Tiago.py
@@ -1,3 +1,16 @@
+tipos_desastre = []
+paises = []
+cidades = []
+bairros = []
+ruas = []
+
+total_afetados = []
+criancas = []
+adultos = []
+idosos = []
+mobilidade_reduzida = []
+feridos = []
+
 quantidade = int(input("Quantos desastres foram registrados? "))
 
 for i in range(quantidade):
@@ -8,6 +21,12 @@ for i in range(quantidade):
     cidade = input("Em qual cidade: ")
     bairro = input("Em qual bairro: ")
     rua = input("Em qual rua: ")
+
+    tipos_desastre.append(tipo)
+    paises.append(pais)
+    cidades.append(cidade)
+    bairros.append(bairro)
+    ruas.append(rua)
 
     while True:
         total = int(input("Quantidade TOTAL de pessoas afetadas: "))
@@ -20,15 +39,74 @@ for i in range(quantidade):
         soma = qtd_criancas_afet + qtd_adultos_afet + qtd_idosos_afet + qtd_mobilidade_afet + qtd_feridos_afet
 
         if soma == total:
-            print("Tudo certo")
+            total_afetados.append(total)
+            criancas.append(qtd_criancas_afet)
+            adultos.append(qtd_adultos_afet)
+            idosos.append(qtd_idosos_afet)
+            mobilidade_reduzida.append(qtd_mobilidade_afet)
+            feridos.append(qtd_feridos_afet)
+            break
         else:
             print("\nERRO: A soma das categorias NÃO corresponde ao total de pessoas afetadas.")
             print(f"\nSoma das categorias = {soma}, mas o total informado foi {total}")
             print("\nPor favor, preencha os dados corretamente")
-            break
 
+print("\n\n== RELATÓRIO DOS DESASTRES REGISTRADOS ==")
+for i in range(quantidade):
+    print(f"\n Desastre {i + 1} ")
+    print(f"Tipo: {tipos_desastre[i]}")
+    print(f"Rua {ruas[i]} ")
+    print(f"Bairro: {bairros[i]} ")
+    print(f"Cidade: {cidades[i]} ")
+    print(f"País: {paises[i]}")
+    print(f"Total de pessoas afetadas: {total_afetados[i]}")
+    print(f"  - Crianças: {criancas[i]}")
+    print(f"  - Adultos: {adultos[i]}")
+    print(f"  - Idosos: {idosos[i]}")
+    print(f"  - Pessoas com mobilidade reduzida: {mobilidade_reduzida[i]}")
+    print(f"  - Feridos: {feridos[i]}")
 
+# relatorio
 
+# A-)
+print(f"\nQuantidade de desastres registrados:{quantidade}")
 
+# B-)
+total_geral_afetados = sum(total_afetados)
+print(f" Total geral de pessoas afetadas: {total_geral_afetados}")
 
+# C-)
+total_criancas = sum(criancas)
+total_adultos = sum(adultos)
+total_idosos = sum(idosos)
+total_mobilidade = sum(mobilidade_reduzida)
+total_feridos = sum(feridos)
 
+print(" Total por categoria:")
+print(f"  - Crianças: {total_criancas}")
+print(f"  - Adultos: {total_adultos}")
+print(f"  - Idosos: {total_idosos}")
+print(f"  - Pessoas com mobilidade reduzida: {total_mobilidade}")
+print(f"  - Feridos: {total_feridos}")
+
+# D-)
+categorias = {
+    "Crianças": total_criancas,
+    "Adultos": total_adultos,
+    "Idosos": total_idosos,
+    "Pessoas com mobilidade reduzida": total_mobilidade,
+    "Feridos": total_feridos
+}
+
+categoria_mais_afetada = max(categorias, key=categorias.get)
+print(f"\nCategoria mais afetada no geral: {categoria_mais_afetada} ({categorias[categoria_mais_afetada]} pessoas)")
+
+# E-)
+maior_afetados = max(total_afetados)
+indice_maior = total_afetados.index(maior_afetados)
+
+print("\nDesastre com maior número de afetados:")
+print(f"  - Tipo: {tipos_desastre[indice_maior]}")
+print(
+    f"  - Localização: País:{paises[indice_maior]} | Cidade: {cidades[indice_maior]} | Bairro: {bairros[indice_maior]} | Rua: {ruas[indice_maior]}")
+print(f"  - Total de afetados: {maior_afetados}")
